84610b5d9a94ea52ea935586bbc252436230281d
faithk7 <faithk7u@outlook.com>
2023-11-02 04:48:21 -0400

    added an example of running tensorflow

---
+++ tensorflow_example.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+
+model = tf.keras.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+model.fit(x_train, y_train, epochs=5)
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print(f"\nTest accuracy: {test_acc}")
